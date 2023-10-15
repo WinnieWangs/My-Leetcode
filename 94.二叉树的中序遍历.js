036abc9ac0fc304c6e327e98fdaa859cece3091c
JinTianBuXiangShangBan <shadow6217@gmail.com>
2023-10-15 12:22:09 +0800

    Create 94.二叉树的中序遍历.js

---
+++ 94.二叉树的中序遍历.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=javascript
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    const res = []
+
+    const traverse = node => {
+        if (!node) return
+        traverse(node.left)
+        res.push(node.val)
+        traverse(node.right)
+    }
+
+    return (traverse(root), res)
+};
+// @lc code=end
+
