54703b1bd1e4199a63ec6c06919888fa94ed6bdf
JinTianBuXiangShangBan <shadow6217@gmail.com>
2023-10-17 10:33:04 +0800

    Create 199.二叉树的右视图.js

---
+++ 199.二叉树的右视图.js
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=199 lang=javascript
+ *
+ * [199] 二叉树的右视图
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+    const res = []
+    if (root === null) return res
+
+    const queue = [root]
+
+    while(queue.length) {
+        const len = queue.length
+
+        for(let i = 0; i < len; i++) {
+            const node = queue.shift()
+            if (node.left !== null) queue.push(node.left)
+            if (node.right !== null) queue.push(node.right)
+
+            if (i === len - 1) res.push(node.val)
+        }
+    }
+
+    return res
+};
+// @lc code=end
+
