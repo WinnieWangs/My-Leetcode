a0b8b1fec5c21e6a3c66e7ce8788e8723bc70e69
JinTianBuXiangShangBan <shadow6217@gmail.com>
2023-10-19 09:30:59 +0800

    Create 117.填充每个节点的下一个右侧节点指针-ii.js

---
+++ 117.填充每个节点的下一个右侧节点指针-ii.js
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=117 lang=javascript
+ *
+ * [117] 填充每个节点的下一个右侧节点指针 II
+ */
+
+// @lc code=start
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function(root) {
+    if(root === null) return null
+
+    const queue = [root]
+
+    while(queue.length > 0) {
+        const len = queue.length
+
+        let head = null
+
+        for(let i = 0; i < len; i++) {
+            const node = queue.shift()
+
+            if (head !== null) head.next = node
+            if (node.left !== null) queue.push(node.left)
+            if (node.right !== null) queue.push(node.right)
+
+            head = node
+        }
+    }
+
+    return root
+};
+// @lc code=end
+
