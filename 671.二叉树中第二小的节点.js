a3176b9fd6f44238bf7405f09c1923f7ed86e2c8
JinTianBuXiangShangBan <shadow6217@gmail.com>
2023-10-20 10:31:41 +0800

    Create 671.二叉树中第二小的节点.js

---
+++ 671.二叉树中第二小的节点.js
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode.cn id=671 lang=javascript
+ *
+ * [671] 二叉树中第二小的节点
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+// var findSecondMinimumValue = function (root) {
+//     if (!root) return -1
+//     const min = root.val
+//     let res = -1
+
+//     const dfs = node => {
+//         if (node === null) return
+
+//         const { val, left, right } = node
+
+//         if (res !== -1 && val >= res) return
+
+//         if (val > min) res = val
+
+//         dfs(left)
+//         dfs(right)
+//     }
+
+//     dfs(root)
+
+//     return res
+// };
+
+var findSecondMinimumValue = function (root) {
+    let res = Number.MAX_SAFE_INTEGER
+    if (!root) return res
+    let queue = [root]
+
+    while(queue.length) {
+        const len = queue.length
+
+        for (let i = 0; i < len; i++) {
+            const { val, left, right } = queue.shift()
+
+            if (val > root.val) {
+                res = Math.min(res, val)
+            }
+
+            if (left) queue.push(left)
+            if (right) queue.push(right)
+        }
+    }
+
+    return res === Number.MAX_SAFE_INTEGER ? -1 : res
+};
+// @lc code=end
+
