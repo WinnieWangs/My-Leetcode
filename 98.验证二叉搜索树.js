d9c3f2a96076824ed44adee28d4494a15e13be91
JinTianBuXiangShangBan <shadow6217@gmail.com>
2023-10-24 00:04:30 +0800

    Update 98.验证二叉搜索树.js

--- 98.验证二叉搜索树.js
+++ 98.验证二叉搜索树.js
@@ -17,11 +17,47 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
+// var isValidBST = function (root, min = Number.MIN_SAFE_INTEGER, max = Number.MAX_SAFE_INTEGER) {
+//     if (root === null) return true
+//     if (root.val <= min || root.val >= max) return false
+//     return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max)
+// };
 
-var isValidBST = function (root, min = Number.MIN_SAFE_INTEGER, max = Number.MAX_SAFE_INTEGER) {
+const morris = (root, prev, flag) => {
+    let cur = root, mostRight = null
+    while (cur !== null) {
+        mostRight = cur.left
+
+        if (mostRight !== null) {
+            while (mostRight.right !== null && mostRight.right !== cur) {
+                mostRight = mostRight.right
+            }
+            if (mostRight.right === null) {
+                mostRight.right = cur
+                cur = cur.left
+                continue
+            }
+
+
+            mostRight.right = null
+        }
+
+        if (prev >= cur.val) {
+            flag = false
+            break
+        }
+
+        prev = cur.val
+        cur = cur.right
+    }
+
+    return flag
+}
+
+const isValidBST = root => {
     if (root === null) return true
-    if (root.val <= min || root.val >= max) return false
-    return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max)
-};
+
+    return morris(root, Number.MIN_SAFE_INTEGER, true)
+}
 // @lc code=end
 
