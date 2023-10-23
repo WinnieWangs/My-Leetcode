3176b99d36656b17e1e508411820b3235e6e255f
JinTianBuXiangShangBan <shadow6217@gmail.com>
2023-10-23 22:35:28 +0800

    Create 98.验证二叉搜索树.js

---
+++ 98.验证二叉搜索树.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=98 lang=javascript
+ *
+ * [98] 验证二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+
+var isValidBST = function (root, min = Number.MIN_SAFE_INTEGER, max = Number.MAX_SAFE_INTEGER) {
+    if (root === null) return true
+    if (root.val <= min || root.val >= max) return false
+    return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max)
+};
+// @lc code=end
+
