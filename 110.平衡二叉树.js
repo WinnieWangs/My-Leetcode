5efcb55b1c916f0338fbe43ab6b2c5913ea609da
JinTianBuXiangShangBan <shadow6217@gmail.com>
2023-10-26 15:48:10 +0800

    Create 110.平衡二叉树.js

---
+++ 110.平衡二叉树.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=javascript
+ *
+ * [110] 平衡二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function (root) {
+   const getMaxDepth = node => {
+      if (!node) return 0;
+      const left = getMaxDepth(node.left);
+      const right = getMaxDepth(node.right);
+
+      if (Math.abs(left - right) > 1 || left === -1 || right === -1) {
+        return -1
+      }
+
+      return Math.max(left, right) + 1;
+   }
+
+   return getMaxDepth(root) !== -1
+};
+// @lc code=end
+
