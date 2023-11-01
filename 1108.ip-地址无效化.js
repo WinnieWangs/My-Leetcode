9b0b323f58e6fa42ac77dbb7366f03eaee3c8de9
JinTianBuXiangShangBan <shadow6217@gmail.com>
2023-11-01 15:57:38 +0800

    Create 1108.ip-地址无效化.js

---
+++ 1108.ip-地址无效化.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=1108 lang=javascript
+ *
+ * [1108] IP 地址无效化
+ */
+
+// @lc code=start
+/**
+ * @param {string} address
+ * @return {string}
+ */
+var defangIPaddr = function(address) {
+    if (!address) return ''
+    return address.replace(/\./g, '[.]')
+};
+// @lc code=end
+
