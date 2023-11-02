b186122c5ac1d50fd78c8037c73ec6dde9d0fce4
JinTianBuXiangShangBan <shadow6217@gmail.com>
2023-11-02 10:46:38 +0800

    Create 37.解数独.js

---
+++ 37.解数独.js
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode.cn id=37 lang=javascript
+ *
+ * [37] 解数独
+ */
+
+// @lc code=start
+/**
+ * @param {character[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+var solveSudoku = function (board) {
+    function isValid(row, col, val) {
+        let len = board.length
+        // 行不能重复
+        for (let i = 0; i < len; i++) {
+            if (board[row][i] === val) {
+                return false
+            }
+        }
+        // 列不能重复
+        for (let i = 0; i < len; i++) {
+            if (board[i][col] === val) {
+                return false
+            }
+        }
+        let startRow = Math.floor(row / 3) * 3
+        let startCol = Math.floor(col / 3) * 3
+
+        for (let i = startRow; i < startRow + 3; i++) {
+            for (let j = startCol; j < startCol + 3; j++) {
+                if (board[i][j] === val) {
+                    return false
+                }
+            }
+        }
+
+        return true
+    }
+
+    function backTracking() {
+        for (let i = 0; i < 9; i++) {
+            for (let j = 0; j < 9; j++) {
+                if (board[i][j] !== '.') continue
+                for (let val = 1; val <= 9; val++) {
+                    if (isValid(i, j, `${val}`)) {
+                        board[i][j] = `${val}`
+                        if (backTracking()) return true
+
+                        board[i][j] = `.`
+                    }
+                }
+                return false
+            }
+        }
+        return true
+    }
+    backTracking()
+    return board
+
+};
+// @lc code=end
+
