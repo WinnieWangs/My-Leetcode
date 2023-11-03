61c427c241cc9587462f45b806392b0c9046bf66
JinTianBuXiangShangBan <shadow6217@gmail.com>
2023-11-03 16:46:43 +0800

    Update 117.填充每个节点的下一个右侧节点指针-ii.js

--- 117.填充每个节点的下一个右侧节点指针-ii.js
+++ 117.填充每个节点的下一个右侧节点指针-ii.js
@@ -19,21 +19,22 @@
  * @param {Node} root
  * @return {Node}
  */
-var connect = function(root) {
-    if(root === null) return null
+var connect = function (root) {
+    if (root === null) return null
 
     const queue = [root]
 
-    while(queue.length > 0) {
+    while (queue.length) {
         const len = queue.length
-
         let head = null
 
-        for(let i = 0; i < len; i++) {
+        for (let i = 0; i < len; i++) {
             const node = queue.shift()
 
             if (head !== null) head.next = node
+
             if (node.left !== null) queue.push(node.left)
+
             if (node.right !== null) queue.push(node.right)
 
             head = node
