60042a08f09a04079aff42b4a49656cf24695492
JinTianBuXiangShangBan <shadow6217@gmail.com>
2023-11-11 01:10:56 +0800

    Update 136.只出现一次的数字.js

--- 136.只出现一次的数字.js
+++ 136.只出现一次的数字.js
@@ -10,12 +10,13 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    if (nums.length === 1) return nums[0];
-    nums = nums.sort();
+    let res = 0
 
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== nums[i - 1] && nums[i] !== nums[i + 1]) return nums[i]
+    for(let num of nums) {
+        res = res^num
     }
+
+    return res
 };
 // @lc code=end
 
