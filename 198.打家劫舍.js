d22d4102a7792974a7b8615a7827c793e19b16fb
JinTianBuXiangShangBan <shadow6217@gmail.com>
2023-11-16 09:21:36 +0800

    Update 198.打家劫舍.js

--- 198.打家劫舍.js
+++ 198.打家劫舍.js
@@ -10,8 +10,7 @@
  * @return {number}
  */
 const rob = nums => {
-    if (nums.length === 1) return nums[0]
-    const dp = [nums[0], Math.max(nums[0], nums[1])]
+    const dp = [nums[0], Math.max(nums[0], nums[1] ?? 0)]
 
     for (let i = 2; i < nums.length; i++) {
         dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i])
